ce51d26e8362b1d18271c247606e8eb65a20be4a
Webrowse <great.adarsh@gmail.com>
2024-10-14 23:31:09 +0530

    finish keyevent and mouse event

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -4,6 +4,25 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
 
-for(let i=0; i< btnsOpenModal.length; i++) console.log(btnsOpenModal[i].textContent);
+const openModal = function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModel = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for(let i=0; i< btnsOpenModal.length; i++) 
+    btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModel);
+overlay.addEventListener('click', closeModel);
+
+document.addEventListener('keydown', function(e){
+   if(e.key === 'Escape' && !modal.classList.contains('hidden')){
+        closeModel();
+   } 
+});
